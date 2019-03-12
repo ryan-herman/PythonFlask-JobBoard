42c67949dc26c0ef46b5438b5249db0704416d77
ryan-herman <47902796+ryan-herman@users.noreply.github.com>
2019-03-12 15:18:28 -0400

    Prep for Dynamic Content
    Added global database and functions to open/close db connection & query db

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,41 @@
 ## IMPORTS
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+## CONSTANTS
+PATH = 'db/jobs.sqlite' 
 
 ## Create Flask App
 app = Flask(__name__)
+
+## Global Database Attribute
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = sqlite3.connect(PATH)
+        g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+## Database Query
+def execute_sql(sql, values = (), commit = False, single = False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+## Close Connection Decorator
+@app.teardown_appcontext
+## Close Database Connection
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection != None:
+        connection.close()
 
 ## Index Route Decorators
 @app.route('/')
